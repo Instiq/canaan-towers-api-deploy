a752b9e08b820c7f1b84bcd0d2f876a27c08d199
blessedaudu <adududuke@gmail.com>
2020-11-27 16:13:01 +0100

    update

--- src/index.js
+++ src/index.js
@@ -20,6 +20,8 @@ const app = express();
 const port = process.env.PORT;
 
 app.use((req, res, next) => {
+    req.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next()
