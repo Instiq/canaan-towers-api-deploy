fca41b0839db8430e6b2ab0082a26c87878e625e
blessedaudu <adududuke@gmail.com>
2020-12-01 16:45:24 +0100

    whitelisting origins

--- src/index.js
+++ src/index.js
@@ -31,16 +31,8 @@ const port = process.env.PORT;
 
 app.use(helmet());
 app.options('*', cors());
-
-var whitelist = ['https://canaantowersb.netlify.app', 'http://localhost:8080', 'http://localhost:8081']
 app.use(cors( {
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-          callback(null, true)
-        } else {
-          callback(new Error('Not allowed by CORS'))
-        }
-    },
+    origin: 'https://canaantowersb.netlify.app',
     exposedHeaders: [ 'Authorization' ]
 }))
 
