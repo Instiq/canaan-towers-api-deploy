cf7178f90203f22999af2768cc4bbb98f5ccf336
blessedaudu <adududuke@gmail.com>
2020-12-02 05:19:57 +0100

    update

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ app.use(cors({
     if(!origin) return callback(null, true);
     if(allowedOrigins.indexOf(origin) === -1){
       var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
+                'allow access from the specified Origin';
       return callback(new Error(msg), false);
     }
     return callback(null, true);
